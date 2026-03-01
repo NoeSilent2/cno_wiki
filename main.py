a097e925d0a17f103bca30334606871a1b412985
NoeSilent2 <ezekielr2006@gmail.com>
2026-03-01 15:21:30 -0500

    first commit
    seeing what i can change :^)

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def read_root():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Vercel + Flask</title>
+        <title>C&O Wiki</title>
         <link rel="icon" type="image/svg+xml" href="/favicon.ico">
         <style>
             * { margin: 0; padding: 0; box-sizing: border-box; }
