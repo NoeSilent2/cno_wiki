31571689f5d9eec989c49e947a60e8de105e5360
NoeSilent2 <ezekielr2006@gmail.com>
2026-03-02 13:01:58 -0500

    pushing routes into main
    no idea what endpoints is normally meant to be used for, but its not what i need so its all going into main

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
-from endpoints import api_bp
 
 
 app = Flask(__name__)
 
 
-app.register_blueprint(api_bp)
-
-
 @app.route("/")
 def read_root():
     return render_template("main.html")
+
+testdata = [
+    {"id": 1026, "name": "Bamzu", "type1": "Grass", "type2": ""},
+    {"id": 1027, "name": "Bambud", "type1": "Grass", "type2": "Steel"},
+    {"id": 1028, "name": "Bamnom", "type1": "Grass", "type2": "Steel"}
+]
+
+@app.route("/species")
+def get_species():
+    return render_template("species.html", species=testdata)
