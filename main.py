ab7982e30691df24a85b1638b0c886980970718e
NoeSilent2 <ezekielr2006@gmail.com>
2026-03-03 00:32:31 -0500

    adding full json data
    hopefully this reads and displays like i am imagining it. probably wont

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+import json
 from flask import Flask, render_template
 
 
@@ -8,12 +10,9 @@ app = Flask(__name__)
 def read_root():
     return render_template("main.html")
 
-testdata = [
-    {"id": 1026, "name": "Bamzu", "type1": "Grass", "type2": ""},
-    {"id": 1027, "name": "Bambud", "type1": "Grass", "type2": "Steel"},
-    {"id": 1028, "name": "Bamnom", "type1": "Grass", "type2": "Steel"}
-]
+with open('./data/speciesdata.json', 'r', encoding='utf-8') as file:
+    speciesdata = json.load(file)
 
 @app.route("/species")
 def get_species():
-    return render_template("species.html", species=testdata)
+    return render_template("species.html", species=speciesdata)
