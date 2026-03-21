389afe73ee51b7b371eb3701c439587cc376c390
NoeSilent2 <ezekielr2006@gmail.com>
2026-03-21 13:05:56 -0400

    reordering a bunch of stuff

--- static/js/species-specific.js
+++ static/js/species-specific.js
@@ -22,12 +22,10 @@ async function get_moves() {
     return data.data
 }
 
-async function enhanceLearnset() {
-    const learnsets = document.querySelectorAll('tbody#learnset-tbody');
-    if (!learnsets) return;
-
+async function enhance_page() {
     let moves = await get_moves();
-    
+
+    const learnsets = document.querySelectorAll('tbody#learnset-tbody');
     learnsets.forEach(tbody => {
         const rows = tbody.querySelectorAll('tr');
         if (rows.length === 0) return;
@@ -51,10 +49,61 @@ async function enhanceLearnset() {
                 row.innerHTML = `<td colspan="6" style="color:red">Unknown move: ${moveId}</td>`;
             }
         });
+    });
+
+    document.querySelectorAll("button#collapseHead").forEach(butt => {
+        const collapseRow = butt.nextElementSibling.querySelector('tr#collapseRow')
+
+        butt.addEventListener('click', () => {
+            if (collapseRow.style.height == "auto") {
+                collapseRow.style.height = 0
+            } else {
+                collapseRow.style.height = "auto"
+            }
+        })
     })
+
+    document.querySelectorAll("model-viewer#pokemon").forEach(modelViewerObject => {
+        const shinyToggle = modelViewerObject.querySelector('input[type="checkbox"]');
+        let modelLoaded = false;
+
+        shinyToggle.addEventListener('change', () => {
+            if (modelLoaded) {
+                return
+            }
+            const imagename = shinyToggle.attributes.imagename.value;
+            if (shinyToggle.checked) {
+                modelViewerObject.poster =`https://img.pokemondb.net/sprites/home/shiny/${imagename}.png`
+            } else {
+                modelViewerObject.poster =`https://img.pokemondb.net/sprites/home/normal/${imagename}.png`
+            }
+        });
+
+        modelViewerObject.addEventListener("load", () => {
+            modelLoaded = true
+
+            modelViewerObject.variantName = "default";
+
+            shinyToggle.addEventListener('change', () => {
+                if (shinyToggle.checked) {
+                    modelViewerObject.variantName = "shiny";
+                } else {
+                    modelViewerObject.variantName = "default";
+                }
+            })
+
+            for (const animName of modelViewerObject.availableAnimations) {
+                if (animName.toLowerCase().includes('idle') && !animName.toLowerCase().includes('ride')) {
+                    modelViewerObject.animationName = animName;
+                    modelViewerObject.play();
+                    break
+                }
+            }
+        });
+    });
 }
 
 // Run when page loads
 document.addEventListener('DOMContentLoaded', () => {
-    enhanceLearnset();
+    enhance_page();
 });
